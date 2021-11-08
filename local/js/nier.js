324ce7119f373f902775db72285d6602317256b9
VitalD <vb@studio-88.ru>
2021-11-08 16:20:28 +0300

    Added Sign In feature

--- local/js/nier.js
+++ local/js/nier.js
@@ -2,6 +2,8 @@
 
 import * as nearAPI from "/node_modules/near-api-js/dist/near-api-js.min.js";
 
+const contractName = 'nft.battlemon.testnet';
+
 const config = {
   networkId: "testnet",
   nodeUrl: "https://rpc.testnet.near.org",
@@ -14,18 +16,20 @@ const near = await nearApi.connect(config);
 
 const wallet = new nearApi.WalletConnection(near);
 
+console.log(wallet.isSignedIn());
+
 const account = await near.account("nft.battlemon.testnet");
 
 const contract = new nearApi.Contract(
-  account,
-  "nft.battlemon.testnet",
+  account, contractName,
   {
-    viewMethods: ["nft_total_supply", "nft_tokens"],
+    viewMethods: ["nft_total_supply", "nft_tokens", "buy"],
     sender: account,
   }
 );
 
 contract.nft_tokens().then(data => {
+	console.log(data);
 	if(data != null) {
 		jsNftList.innerHTML = '';
 
@@ -50,3 +54,16 @@ contract.nft_tokens().then(data => {
 		jsNftList.innerHTML = newH;
 	}
 });
+
+if(wallet.isSignedIn()) {
+	const accountId = wallet.getAccountId();
+	document.querySelectorAll('.login-form-toggle span').forEach(loginBtn => {
+		loginBtn.innerHTML = accountId
+	});
+} else {
+	document.querySelectorAll('.login-form-toggle').forEach(loginBtn => loginBtn.addEventListener('click', function() {
+	  wallet.requestSignIn(
+	    contractName,
+	  );
+	}));
+}
